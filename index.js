d54762675468b352a221a5846038a3a94d1e4a31
HarshitKhetawat <harshit.k@mysathi.in>
2025-09-10 16:34:01 +0530

    frontend url added in index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "https://r8752nt4-5173.inc1.devtunnels.ms",
-      "https://mass-mail-dispatcher-frontend.vercel.app/",
+      "https://mass-mail-dispatcher-frontend.vercel.app",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
